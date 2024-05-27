dd9503eadc982f1cb62c41e85887b7785ee9460f
itzhang89 <itzhang89@163.com>
2024-05-27 10:40:03 +0800

    add a install script

---
+++ setup_docker_alias.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 下载 .docker_alias 文件
+curl -LO https://raw.githubusercontent.com/itzhang89/docker-alias/main/.docker_alias
+
+# 检查文件是否成功下载
+if [ -f .docker_alias ]; then
+    # 将其添加到 ~/.bashrc 中，并立即生效
+    echo "[ -f ~/.docker_alias ] && source ~/.docker_alias" >> ~/.bashrc
+    source ~/.bashrc
+    echo "Docker aliases have been successfully added and sourced."
+else
+    echo "Failed to download .docker_alias file."
+fi
+
