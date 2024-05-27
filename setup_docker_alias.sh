666857cfdb0eadd9fdfb59ee09c01ad8965a7a57
itzhang89 <30542109+itzhang89@users.noreply.github.com>
2024-05-27 10:42:41 +0800

    Update setup_docker_alias.sh

--- setup_docker_alias.sh
+++ setup_docker_alias.sh
@@ -8,7 +8,7 @@ if [ -f .docker_alias ]; then
     # 将其添加到 ~/.bashrc 中，并立即生效
     echo "[ -f ~/.docker_alias ] && source ~/.docker_alias" >> ~/.bashrc
     source ~/.bashrc
-    echo "Docker aliases have been successfully added and sourced."
+    echo "Docker aliases have been successfully added and sourced. restart the terminal or run the source ~/.bashrc in the terminal"
 else
     echo "Failed to download .docker_alias file."
 fi
